3b5ed1710e4150ad1084b9a5c20341e946f69e0d
Geoff Schmit <gcschmit@gmail.com>
2025-09-02 08:47:58 -0500

    Start example of sets.

--- Chapter 15 Class Notes/src/WordAnalysis.java
+++ Chapter 15 Class Notes/src/WordAnalysis.java
@@ -13,6 +13,34 @@ public class WordAnalysis
     public static void main(String[] args)
         throws FileNotFoundException
     {
+        Set<String> dictionaryWords = readWords("src/words");
+        Set<String> novelWords = readWords("src/throughTheLookingGlass.txt");
+
+        // 1. print all the words that are in the novel but not the dictionary
+
+        // the enhanced for loop works with sets
+        for(String word : novelWords)
+        {
+            if(!dictionaryWords.contains(word))
+            {
+                System.out.println(word);
+            }
+        }
+
+        // 2. print the number of unique words in the novel
+        System.out.println("unique words: " + novelWords.size());
+
+        // 3. print the number of unique words with > 3 letters
+        Iterator<String> i = novelWords.iterator();
+        while(i.hasNext())
+        {
+            if(i.next().length() <= 3)
+            {
+                i.remove();
+            }
+        }
+
+        System.out.println("unique words (> 3 letters)" + novelWords.size());
     }
 
     /**
