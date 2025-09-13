defaf06f8cba75a7641b6d92df63f289a3ae36e8
Geoff Schmit <gcschmit@gmail.com>
2025-09-13 17:02:09 -0500

    Add notes on the Stack class.

--- Chapter 15 Class Notes/src/StackDemo.java
+++ Chapter 15 Class Notes/src/StackDemo.java
@@ -9,5 +9,27 @@ public class StackDemo
 {
     public static void main(String[] args)
     {
+        // create a stack of commands
+        Stack<String> commands = new Stack<>();
+
+        // push a bunch of commands onto the undo stack
+        commands.push("Insert: 'Hello'");
+        commands.push("Insert: ','");
+        commands.push("Insert: ' '");
+        commands.push("Insert: 'World'");
+        commands.push("Insert: '?'");
+        commands.push("Delete: '?'");
+        commands.push("Insert: '!'");
+
+        // print the stack; the top of the stack is on the far right
+        System.out.println(commands);
+
+        // simulate the user pressing undo 4 times
+        for(int i = 0; i < 4; i++)
+        {
+            String command = commands.pop();
+            System.out.println("Undo: " + command);
+        }
+
     }
 }
