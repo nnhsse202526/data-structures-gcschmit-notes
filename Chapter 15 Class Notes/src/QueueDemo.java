36e35b617a61c66528746cdd515d1a4c39d71718
Geoff Schmit <gcschmit@gmail.com>
2025-09-15 09:32:54 -0500

    Add example for queues.

--- Chapter 15 Class Notes/src/QueueDemo.java
+++ Chapter 15 Class Notes/src/QueueDemo.java
@@ -9,5 +9,26 @@ public class QueueDemo
 {
     public static void main(String[] args)
     {
+        // create a print queue of strings (using a linked list)
+        Queue<String> jobs = new LinkedList<>();
+
+        // add several print jobs
+        jobs.add("Joe: junior photos");
+        jobs.add("Cathy: faculty photos");
+
+        System.out.println("Printing: " + jobs.remove());
+
+        jobs.add("Cathy: XC team photo");
+        jobs.add("Joe: senior photos");
+        jobs.add("Joe: sophomore photos");
+
+        System.out.println("Printing: " + jobs.remove());
+
+        jobs.add("Cathy: robotics spread");
+
+        while(jobs.size() > 0)
+        {
+            System.out.println("Printing: " + jobs.remove());
+        }
     }
 }
