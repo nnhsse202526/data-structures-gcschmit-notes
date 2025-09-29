b282f7238d82b956728642088fcba79c6f19478f
Geoff Schmit <gcschmit@gmail.com>
2025-09-29 09:41:35 -0500

    Implement LinkedListIterator.

--- Chapter 16 Class Notes/src/LinkedList.java
+++ Chapter 16 Class Notes/src/LinkedList.java
@@ -73,6 +73,10 @@ public class LinkedList
         Returns an iterator for iterating through this list.
         @return an iterator for iterating through this list
     */
+    public ListIterator listIterator()
+    {
+        return new LinkedListIterator();
+    }
 
     // Class Node
     static class Node
@@ -82,37 +86,87 @@ public class LinkedList
     }
 
 
-    class LinkedListIterator //implements ListIterator
+    class LinkedListIterator implements ListIterator
     {
-      //private data
-
+      // private data
+      private Node position;
+      private Node previous;
+      private boolean isAfterNext;
 
         /**
             Constructs an iterator that points to the front
             of the linked list.
         */
-
+        public LinkedListIterator()
+        {
+            position = null;
+            previous = null;
+            isAfterNext = false;
+        }
 
         /**
             Moves the iterator past the next element.
             @return the traversed element
         */
+        public Object next()
+        {
+            if(hasNext() == false)
+            {
+                throw new NoSuchElementException();
+            }
 
+            previous = position;
+            isAfterNext = true;
 
+            if(position == null)
+            {
+                position = first;
+            }
+            else
+            {
+                position = position.next;
+            }
 
-
+            return position.data;
+        }
 
         /**
             Tests if there is an element after the iterator position.
             @return true if there is an element after the iterator position
         */
+        public boolean hasNext()
+        {
+            if(position == null) // we are at the start of the list
+            {
+                return first != null;
+            }
 
+            return position.next != null;
+        }
 
         /**
             Adds an element before the iterator position
             and moves the iterator past the inserted element.
             @param element the element to add
         */
+        public void add(Object element)
+        {
+            if(position == null)
+            {
+                addFirst(element);
+                position = first;
+            }
+            else
+            {
+                Node newNode = new Node();
+                newNode.data = element;
+                newNode.next = position.next;   // #1
+                position.next = newNode;        // #2
+                position = newNode;             // #3
+            }
+
+            isAfterNext = false;                // #4
+        }
 
 
 
