951fa1b8e9c780ced07a4bf177f63518eaa6419c
Geoff Schmit <gcschmit@gmail.com>
2025-09-29 09:41:03 -0500

    Implement LinkedList.

--- Chapter 16 Class Notes/src/LinkedList.java
+++ Chapter 16 Class Notes/src/LinkedList.java
@@ -8,51 +8,78 @@ import java.util.NoSuchElementException;
 */
 public class LinkedList
 {
-
+    /*
+     * first refers to the first node in this list. If the list
+     *  is empty, first is null.
+     */
+    private Node first;
 
     /**
         Constructs an empty linked list.
     */
-
-
-
+    public LinkedList()
+    {
+        this.first = null;
+    }
 
     /**
         Returns the first element in the linked list.
         @return the first element in the linked list
     */
+    public Object getFirst()
+    {
+        if(this.first == null)
+        {
+            throw new NoSuchElementException();
+        }
 
-
-
+        return this.first.data;
+    }
 
     /**
         Removes the first element in the linked list.
         @return the removed element
     */
+    public Object removeFirst()
+    {
+        if(this.first == null)
+        {
+            throw new NoSuchElementException();
+        }
 
-
-
-
+        Object element = this.first.data;
+        this.first = this.first.next;
+        return element;
+    }
 
     /**
         Adds an element to the front of the linked list.
         @param element the element to add
     */
+    public void addFirst(Object element)
+    {
+        // #1
+        Node newNode = new Node();
+        newNode.data = element;
 
+        // #2
+        newNode.next = this.first;
 
-
-
+        // #3
+        this.first = newNode;
+    }
 
     /**
         Returns an iterator for iterating through this list.
         @return an iterator for iterating through this list
     */
 
-
-
-
-
-    //Class Node
+    // Class Node
+    static class Node
+    {
+        public Object data;
+        public Node next;
+    }
 
 
     class LinkedListIterator //implements ListIterator
